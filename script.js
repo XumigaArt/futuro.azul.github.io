35dffa36cd4eebdfa27f4511bef56f7cf2a91464
XumigaArt <xavier.sousa.98@gmail.com>
2025-01-06 23:43:42 +0000

    update

--- script.js
+++ script.js
@@ -1,52 +1,63 @@
 
-document.getElementById("logo").addEventListener("click", function(){
-    document.getElementById("audio").play();});
+const animal = 'none';
+const modalContent = document.getElementById('modal-content').innerHTML;
 
 
+ document.getElementById('baleiabutton').onclick = function () {
+     const animal = 'baleia'
+     document.getElementById('modal-content').innerHTML = document.getElementById('baleiacontent').innerHTML;
+     modal.style.display = "block";
 
-
-
-
-
-
-
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.popUp')
-        closeModal(modal)
-    })
-})
-
-
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
+     console.log(animal);
 }
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
+document.getElementById('axolotebutton').onclick = function () {
+    const animal = 'axolote'
+    document.getElementById('modal-content').innerHTML = document.getElementById('axolotecontent').innerHTML;
+    modal.style.display = "block";
+
+    console.log(animal);
+}
+document.getElementById('pinguinbutton').onclick = function () {
+    const animal = 'pinguin'
+    document.getElementById('modal-content').innerHTML = document.getElementById('pinguincontent').innerHTML;
+    modal.style.display = "block";
+
+    console.log(animal);
+}
+document.getElementById('turtlebutton').onclick = function () {
+    const animal = 'turtle'
+    document.getElementById('modal-content').innerHTML = document.getElementById('turtlecontent').innerHTML;
+    modal.style.display = "block";
+
+    console.log(animal);
 }
 
+console.log(animal);
 
 
 
 
-//document.getElementById('baleia_popup').style.display = 'none';
+var modal = document.getElementById("myModal");
 
-   // document.getElementById('baleiabtn').onclick = function () {
-   //     document.getElementById('baleia_popup').style.display = 'block';
-   //    }
+// Get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
