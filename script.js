e7ab0d3f24cdc6cd725f9246a89cfc5fc9940c1d
XumigaArt <xavier.sousa.98@gmail.com>
2025-01-06 16:31:35 +0000

    update

--- script.js
+++ script.js
@@ -5,13 +5,48 @@ document.getElementById("logo").addEventListener("click", function(){
 
 
 
-const baleiabtn = document.getElementById('baleiabtn');
-const baleiapopup = document.getElementById('baleia_popup');
 
 
-document.getElementById('baleia_popup').style.display = 'none';
 
-    document.getElementById('baleiabtn').onclick = function () {
-        document.getElementById('baleia_popup').style.display = 'block';
-    }
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.popUp')
+        closeModal(modal)
+    })
+})
+
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+
+
+
+
+//document.getElementById('baleia_popup').style.display = 'none';
+
+   // document.getElementById('baleiabtn').onclick = function () {
+   //     document.getElementById('baleia_popup').style.display = 'block';
+   //    }
 
