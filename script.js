4aebf9df6be0822b48d9e8cf42ff196cf8ea948b
Henryyy7 <henriquejocf@gmail.com>
2025-01-06 00:07:26 +0000

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    <script>
+        const image = document.getElementById('logo');
+        const audio = document.getElementById('audio');
+
+        image.addEventListener('click', () => {
+        audio.play();
+    });
+    </script>
