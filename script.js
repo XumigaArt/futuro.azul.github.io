5f58f3310d7a2f53f3bbe96308ab2ef4dff685e0
Henryyy7 <henriquejocf@gmail.com>
2025-01-06 00:13:35 +0000

    update

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 
     <script>
+        // Get references to the image and audio elements
         const image = document.getElementById('logo');
         const audio = document.getElementById('audio');
 
+
         image.addEventListener('click', () => {
+        if (audio.paused) {
+        audio.play()
+        .catch(error => {
+        console.error("Error playing audio:", error);
+    });
+    } else {
+        audio.currentTime = 0; //
         audio.play();
+    }
     });
     </script>
