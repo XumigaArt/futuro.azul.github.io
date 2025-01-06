e76b789298412e2f67081348063097738b419a19
Henryyy7 <henriquejocf@gmail.com>
2025-01-06 00:38:09 +0000

    update

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var audio = new Audio('imgs/audio/goat-sound-effect-259473.mp3');
-<audio id="audio" src="imgs/audio/goat-sound-effect-259473.mp3"></audio>;
-    document.getElementById("logo").addEventListener("click", function(){
-    document.getElementById("audio").play()});
-
+document.getElementById("logo").addEventListener("click", function(){
+    document.getElementById("audio").play();});
 
