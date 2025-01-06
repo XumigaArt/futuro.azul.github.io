23699c823f6753643e5943715cd8967e8013488d
XumigaArt <xavier.sousa.98@gmail.com>
2025-01-06 14:55:27 +0000

    update

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 document.getElementById("logo").addEventListener("click", function(){
     document.getElementById("audio").play();});
 
+
+
+
+const baleiabtn = document.getElementById('baleiabtn');
+const baleiapopup = document.getElementById('baleia_popup');
+
+
+document.getElementById('baleia_popup').style.display = 'none';
+
+    document.getElementById('baleiabtn').onclick = function () {
+        document.getElementById('baleia_popup').style.display = 'block';
+    }
+
