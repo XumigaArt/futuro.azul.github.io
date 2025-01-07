5a718ed960e8dd453736910a4b17b98836e3c525
XumigaArt <xavier.sousa.98@gmail.com>
2025-01-07 00:01:28 +0000

    update

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+//Navigator
+
+document.getElementById('home').onclick = function () {
+
+}
+
+document.getElementById('nav1').onclick = function () {
+
+}
+
+document.getElementById('nav2').onclick = function () {
+    window.location.href = "https://xumigaart.github.io/futuro.azul.github.io/pages/animals.html";
+}
+
+document.getElementById('nav3').onclick = function () {
+
+}
+
+document.getElementById('nav4').onclick = function () {
+
+}
+
+
+
+
+//Modals Animais
+
 
 const animal = 'none';
 const modalContent = document.getElementById('modal-content').innerHTML;
